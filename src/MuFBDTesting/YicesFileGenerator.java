53341bf162368801cffe37f0a99b865a53fde60c
Rienshaliu <rienshaliu@gmail.com>
2021-12-30 09:58:48 +0900

    Remove unused libraries

--- src/MuFBDTesting/YicesFileGenerator.java
+++ src/MuFBDTesting/YicesFileGenerator.java
@@ -3,28 +3,19 @@ package MuFBDTesting;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
-import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import PLC_related.Connection;
-import PLC_related.DPath;
-import PLC_related.Element;
-import PLC_related.LogicStatement;
 import Structure.TestCase;
-import plcopen.inf.type.group.fbd.IInVariable;
 
 public class YicesFileGenerator {
 	
